07f16040b5489c171b027039be394a1873abfb0a
Kilian <kischoenhaar@gmail.com>
2026-02-16 11:26:37 +0100

    refactor(app): reduce main to composition root

---
+++ src/ui/MenuController.hpp
@@ -0,0 +1,19 @@
+#ifndef UI_MENUCONTROLLER_HPP
+#define UI_MENUCONTROLLER_HPP
+
+#include "../service/EntryService.hpp"
+#include "ConsoleInput.hpp"
+#include "ConsoleRenderer.hpp"
+
+class MenuController {
+  public:
+    MenuController(ConsoleRenderer& renderer, ConsoleInput& input, EntryService& entryService);
+    int starte();
+
+  private:
+    ConsoleRenderer& renderer;
+    ConsoleInput& input;
+    EntryService& entryService;
+};
+
+#endif
